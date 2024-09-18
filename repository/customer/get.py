0b102eb2164aea496e179a8a2394b4654dad6ee5
chrslex <alexandro.tobing@gmail.com>
2024-09-19 00:15:23 +0700

    maintain kv in json response

--- repository/customer/get.py
+++ repository/customer/get.py
@@ -1,5 +1,6 @@
 from flask import jsonify, g
 from repository.customer import customer_bp
+from collections import OrderedDict
 
 # READ all customers
 @customer_bp.route('/', methods=["GET"])
@@ -12,4 +13,24 @@ def get_customers():
     cursor.close()
     connection.close()
 
-    return jsonify(customers)
+    if customers:
+        data_dict = map_to_dict(customers)
+        return jsonify(data_dict)
+    else:
+        return jsonify({"error": "User not found"}), 404
+    
+   
+
+
+def map_to_dict(customers):
+    res = []
+    keys = [
+        "id", "title", "name", "gender", "phone_number",
+        "image", "email", "created_at", "updated_at"
+    ]
+
+    for c in customers:
+        temp = OrderedDict(zip(keys, c))
+        res.append(temp)
+
+    return res
