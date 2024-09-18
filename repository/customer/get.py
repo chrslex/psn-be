b9e831db04867e0d2c2294c1cf54cc8b666e8b96
chrslex <alexandro.tobing@gmail.com>
2024-09-19 00:31:11 +0700

    f: specific customer without address

--- repository/customer/get.py
+++ repository/customer/get.py
@@ -5,32 +5,27 @@ from collections import OrderedDict
 # READ all customers
 @customer_bp.route('/', methods=["GET"])
 def get_customers():
-    connection  = g.db_connection
-    cursor = connection.cursor()
+    connection = g.db_connection
+    cursor = connection.cursor(dictionary=True)
     
-    cursor.execute("SELECT * FROM Customer")
+    cursor.execute("SELECT ID, title, name, gender, phone_number, image, email, created_at, updated_at FROM Customer")
     customers = cursor.fetchall()
     cursor.close()
     connection.close()
 
-    if customers:
-        data_dict = map_to_dict(customers)
-        return jsonify(data_dict)
-    else:
-        return jsonify({"error": "User not found"}), 404
+    return jsonify(customers)
+
+@customer_bp.route("/<int:id>", methods=["GET"])
+def get_customer_detail(id):
+    connection = g.db_connection
+    cursor = connection.cursor(dictionary=True)
     
-   
+    cursor.execute("SELECT * FROM Customer WHERE ID = %s", (id,))
+    customer = cursor.fetchone()
+    cursor.close()
+    connection.close()
 
-
-def map_to_dict(customers):
-    res = []
-    keys = [
-        "id", "title", "name", "gender", "phone_number",
-        "image", "email", "created_at", "updated_at"
-    ]
-
-    for c in customers:
-        temp = OrderedDict(zip(keys, c))
-        res.append(temp)
-
-    return res
+    if customer:
+        return jsonify(customer)
+    else:
+        return jsonify({'error': 'Customer not found'}), 404
